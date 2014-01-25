1f09df1118f41dac49a13ef710ffb1d42c8d12ed
George Treacy <george@huzco.com>
2014-01-25 18:35:35 +1000

    works with static json file

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,18 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+
+  .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+	
+	$http.get('data/phones.json').success(function(data) {
+    	$scope.phones = data;
+  	});
 
   }])
+
+
+
   .controller('MyCtrl2', [function() {
 
   }]);
